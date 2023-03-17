2f02dcc36dc7b9437fbffc5fe54392d5c6eb0b72
Luis Fernando Vazquez Caracas <lvasquca@emeal.nttdata.com>
2023-03-17 10:09:19 +0100

    Add some tests

--- src/components/phone-card/phone-card.spec.jsx
+++ src/components/phone-card/phone-card.spec.jsx
@@ -4,8 +4,11 @@ import { render } from '@testing-library/react';
 import { PhoneCard } from './phone-card';
 
 describe('PhoneCard', () => {
+  const productMock = jest.mock('./mocks/product.mock.json', () => ({ foo: 'bar' }), {
+    virtual: true,
+  });
   it('should render successfully', () => {
-    const { baseElement } = render(<PhoneCard />);
+    const { baseElement } = render(<PhoneCard product={productMock} />);
     expect(baseElement).toBeTruthy();
   });
 });
--- src/components/phone-colors/phone-colors.spec.jsx
+++ src/components/phone-colors/phone-colors.spec.jsx
@@ -4,8 +4,18 @@ import { render } from '@testing-library/react';
 import { PhoneColors } from './phone-colors';
 
 describe('PhoneColors', () => {
+  const colorsMock = [
+    {
+      code: 1000,
+      name: 'Blue',
+    },
+    {
+      code: 1001,
+      name: 'White',
+    },
+  ];
   it('should render successfully', () => {
-    const { baseElement } = render(<PhoneColors />);
+    const { baseElement } = render(<PhoneColors colors={colorsMock} />);
     expect(baseElement).toBeTruthy();
   });
 });
